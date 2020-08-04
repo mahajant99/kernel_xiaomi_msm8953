4178ecb9ed0e07629f24b5f6f2176fe6445172d5
33bca <fabian.leutenegger@bluewin.ch>
2020-08-04 17:58:21 +0530

    msm: camera-v2_legacy: Import daisy changes
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- drivers/media/platform/msm/camera_v2-legacy/sensor/eeprom/msm_eeprom.c
+++ drivers/media/platform/msm/camera_v2-legacy/sensor/eeprom/msm_eeprom.c
@@ -170,6 +170,16 @@ static int read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl,
 
 	CDBG("%s %d addr [0x0000] = %x, [0x0001] = %x\n", __func__, __LINE__, sensor_id[0], sensor_id[1]);
 
+	e_ctrl->i2c_client.addr_type = 2;
+	rc = e_ctrl->i2c_client.i2c_func_tbl->i2c_read_seq(
+			&(e_ctrl->i2c_client), 0x0000,
+			sensor_id, 2);
+	if (rc < 0) {
+		pr_err("%s %d error\n", __func__, __LINE__);
+		return rc;
+	}
+
+	CDBG("%s %d addr [0x0000] = %x, [0x0001] = %x\n", __func__, __LINE__, sensor_id[0], sensor_id[1]);
 	for (j = 0; j < block->num_map; j++) {
 		if (emap[j].saddr.addr) {
 			eb_info->i2c_slaveaddr = emap[j].saddr.addr;
